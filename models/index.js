58ee0fd86f57c8b21e05eddff0de00b497aa9333
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-05-03 18:53:16 -0500

    Edited the index.js file in the models folder to reflect the relationship between Questions and Answers

--- models/index.js
+++ models/index.js
@@ -1,32 +1,41 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Question = require('./Question');
+const Answer = require('./Answer');
 
 
-User.hasMany(Post, {
-  foreignKey: 'user_id',
+
+Question.hasMany(Answer, {
+  foreignKey: 'question_id',
+}) 
+
+Answer.belongsTo(Question, {
+  foreignKey: 'question_id',
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Post, {
+//   foreignKey: 'user_id',
+// });
+
+// Post.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
 
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Comment, {
+//   foreignKey: 'user_id',
+// });
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+// Comment.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
 
 
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-});
+// Post.hasMany(Comment, {
+//   foreignKey: 'post_id',
+// });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-});
+// Comment.belongsTo(Post, {
+//   foreignKey: 'post_id',
+// });
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Question, Answer };
