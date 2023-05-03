665ba481a196822af8b75e815c323235d2b7caea
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-05-03 18:49:22 -0500

    Update with specific qeustions included from K-I-P app

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,55 @@
-DROP DATABASE IF EXISTS blog_db;
+CREATE DATABASE IF NOT EXISTS `knowledge_is_power`;
 
-CREATE DATABASE blog_db;
+USE `knowledge_is_power`;
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `QuizAnswers`;
+CREATE TABLE `QuizAnswers` (
+    `answer_id` INT NOT NULL AUTO_INCREMENT,
+    `question_id` INT NOT NULL,
+    `answer_choice` TINYINT NOT NULL,
+    `is_correct` TINYINT NOT NULL,
+    PRIMARY KEY (`answer_id`),
+    FOREIGN KEY (`question_id`) REFERENCES `QuizQuestions`(`id`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `QuizQuestions`;
+CREATE TABLE `QuizQuestions` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `question_prompt` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `QuizAnswers` ADD FOREIGN KEY (`question_id`) REFERENCES `QuizQuestions`(`id`);
+ALTER TABLE `Users` ADD FOREIGN KEY (`user_id`) REFERENCES `QuizAnswers`(`answer_id`);
+
+INSERT INTO `QuizQuestions` (`id`, `question_prompt`) VALUES
+(1, 'In the Marvel Comics universe, what is the name of the metal alloy used to create Captain America''s shield?'),
+(2, 'What is the name of the protagonist in the video game series Assassin''s Creed?'),
+(3, 'In the TV show The Big Bang Theory, what is the name of Sheldon''s favorite comic book superhero?'),
+(4, 'What is the name of the planet that the Doctor and his companions often travel to in the TV show Doctor Who?');
+
+INSERT INTO `QuizAnswers` (`question_id`, `answer_choice`, `is_correct`) VALUES
+(1, 1, 0),
+(1, 2, 1),
+(1, 3, 0),
+(1, 4, 0),
+(2, 1, 0),
+(2, 2, 1),
+(2, 3, 0),
+(2, 4, 0),
+(3, 1, 0),
+(3, 2, 1),
+(3, 3, 0),
+(3, 4, 0),
+(4, 1, 0),
+(4, 2, 0),
+(4, 3, 1),
+(4, 4, 0);
--- db/tables.sql
+++ db/tables.sql
@@ -1,59 +1,59 @@
-DROP TABLE IF EXISTS `Users`; 
+CREATE DATABASE IF NOT EXISTS `knowledge_is_power`;
+
+USE `knowledge_is_power`;
+
+DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users` (
-    `user_id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT 0,
-    `username` VARCHAR(20) NULL DEFAULT NULL,
-    `password` VARCHAR(20) NULL DEFAULT NULL,
+    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`user_id`)
-) ENGINE = InnoDB;
+) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `Quiz Answers`;
-CREATE TABLE `Quiz Answers` (
-    `answer_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-    `question_id` INTEGER NULL DEFAULT NULL,
-    `answer_choice` TINYINT NULL DEFAULT NULL,
-    `is_correct` INTEGER NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `QuizAnswers`;
+CREATE TABLE `QuizAnswers` (
+    `answer_id` INT NOT NULL AUTO_INCREMENT,
+    `question_id` INT NOT NULL,
+    `answer_choice` TINYINT NOT NULL,
+    `is_correct` TINYINT NOT NULL,
     PRIMARY KEY (`answer_id`),
-    FOREIGN KEY (`question_id`) REFERENCES `Quiz Questions`(`id`)
-) ENGINE = InnoDB;
+    FOREIGN KEY (`question_id`) REFERENCES `QuizQuestions`(`id`)
+) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `Quiz Questions`;
-CREATE TABLE `Quiz Questions` (
-    `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-    `question_prompt` VARCHAR(255) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `QuizQuestions`;
+CREATE TABLE `QuizQuestions` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `question_prompt` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+) ENGINE=InnoDB;
 
-ALTER TABLE `Quiz Answers` ADD FOREIGN KEY (`question_id`) REFERENCES `Quiz Questions`(`id`);
-ALTER TABLE `Users` ADD FOREIGN KEY (`user_id`) REFERENCES `Quiz Answers`(`answer_id`);
+ALTER TABLE `QuizAnswers` ADD FOREIGN KEY (`question_id`) REFERENCES `QuizQuestions`(`id`);
+ALTER TABLE `Users` ADD FOREIGN KEY (`user_id`) REFERENCES `QuizAnswers`(`answer_id`);
 
-INSERT INTO `Quiz Questions` (`id`,`question_prompt`) VALUES
-(1, 'What year was the first issue of Punk Planet published?'),
-(2, 'Which band is NOT known for being one of the early Hardcore Punk bands?'),
-(3, 'Which of these is NOT a club associated with the early days of Punk Rock?'),
-(4, 'What is the name of the all female punk band that has had hits such as Cherry Bomb and Bad Reputation?'),
-(5, 'Which of these is NOT one of The Ramones original members?');
+INSERT INTO `QuizQuestions` (`id`, `question_prompt`) VALUES
+(1, 'In the Marvel Comics universe, what is the name of the metal alloy used to create Captain America''s shield?'),
+(2, 'What is the name of the protagonist in the video game series Assassin''s Creed?'),
+(3, 'In the TV show The Big Bang Theory, what is the name of Sheldon''s favorite comic book superhero?'),
+(4, 'What is the name of the planet that the Doctor and his companions often travel to in the TV show Doctor Who?');
 
-INSERT INTO `Quiz Answers` (`question_id`,`answer_choice`,`is_correct`) VALUES
-(1,1,1),
-(1,2,0),
-(1,3,0),
-(1,4,0),
-(2,1,0),
-(2,2,0),
-(2,3,1),
-(2,4,0),
-(3,1,0),
-(3,2,0),
-(3,3,0),
-(3,4,1),
-(4,1,0),
-(4,2,1),
-(4,3,0),
-(4,4,0),
-(5,1,0),
-(5,2,0),
-(5,3,0),
-(5,4,1);
+INSERT INTO `QuizAnswers` (`question_id`, `answer_choice`, `is_correct`) VALUES
+(1, 1, 0),
+(1, 2, 1),
+(1, 3, 0),
+(1, 4, 0),
+(2, 1, 0),
+(2, 2, 1),
+(2, 3, 0),
+(2, 4, 0),
+(3, 1, 0),
+(3, 2, 1),
+(3, 3, 0),
+(3, 4, 0),
+(4, 1, 0),
+(4, 2, 0),
+(4, 3, 1),
+(4, 4, 0);
+
 
 
 
