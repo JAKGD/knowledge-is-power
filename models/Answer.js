d7cdb40ebf47ad20b55fba78d2d3a6bd25844cab
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-05-02 21:02:44 -0500

    created Answer.js, User.js, Question.js files

---
+++ models/Answer.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Answer extends Model {}
+
+Answer.init(
+  {
+    question_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'question',
+        key: 'id',
+      },
+    },
+    answer_choice: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+      },
+    is_correct: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,    
+    },
+    answer_key: {
+        primaryKey: true
+
+    }, 
+    
+  },
+  {
+    sequelize,
+    timestamps: true, 
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'answer',
+  }
+);
+
+module.exports = Answer;
---
+++ models/Question.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model { }
+
+Question.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        question_prompt: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'question',
+    }
+);
+
+module.exports = Question;
