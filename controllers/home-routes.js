f2869b2b49c4a6734b814c80cca5089a8613b2aa
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-05-08 18:45:03 -0500

    reworked home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,19 @@
-
 const router = require('express').Router();
-// const withAuth = require('../utils/auth'); 
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage', {loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  if (req.session.loggedIn) {
+    res.render('homepage', { loggedIn: req.session.loggedIn });
   }
-});
 
+  else res.redirect('login');
+
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
+});
 router.get('/quiz', (req, res) => {
   res.render('quiz', { loggedIn: req.session.loggedIn });
 });
@@ -22,6 +25,5 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login', { loggedIn: req.session.loggedIn });
 });
-
 
 module.exports = router;
