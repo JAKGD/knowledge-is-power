f84eb5c9b13824339c97d7ca778b89407654698b
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-05-08 19:06:34 -0500

    Coding to allow for question randomization

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@
 //-- the sequelize variable is exported from the module, allowing it to be used by other modules in the application for querying the database--//
 
 
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 require('dotenv').config(); 
 
 const sequelize = process.env.CLEARDB_DATABASE_URL
   ? new Sequelize(process.env.CLEARDB_DATABASE_URL) 
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
+  : new Sequelize(process.env.DB_NAME, process.env.dkpatzer, process.env.DB_%AngelYukZel78$$, { 
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -18,3 +18,45 @@ const sequelize = process.env.CLEARDB_DATABASE_URL
     });
 
 module.exports = sequelize;
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+*/
+
+//  const Sequelize = require('sequelize');
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+}
+
+module.exports = sequelize;
+
--- controllers/api/question-routes.js
+++ controllers/api/question-routes.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
 const { Question } = require('../../models');
 
+// GET route for retrieving a random question
+router.get('/random', async (req, res) => {
+  try {
+    // Find the total count of questions in the database
+    const count = await Question.count();
+
+    // Generate a random number between 0 and the total count of questions
+    const randomIndex = Math.floor(Math.random() * count);
+
+    // Retrieve a single random question from the database
+    const randomQuestion = await Question.findOne().skip(randomIndex);
+
+    res.status(200).json(randomQuestion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
+
+
+
+// const router = require('express').Router();
+// const { Question } = require('../../models');
+
+
+// module.exports = router;
