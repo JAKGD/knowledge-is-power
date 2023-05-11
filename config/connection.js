8c6c907f2f8c31ecda3968dd4c77f81807e4f407
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-05-10 19:46:26 -0500

    fixed seeding issue, im dummy

--- config/connection.js
+++ config/connection.js
@@ -13,5 +13,6 @@ const sequelize = process.env.JAWSDB_URL
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
+    
   });
 module.exports = sequelize;
--- controllers/api/quiz-routes.js
+++ controllers/api/quiz-routes.js
@@ -23,59 +23,69 @@ router.get('/', async (req, res) => {
   req.session.score = 0;
   // req.session.questionId = question.id;
 
+  // const question_1 = questions[Math.floor(Math.random() * (50))]
+  // const question_2 = questions[Math.floor(Math.random() * (50))]
+  // const question_3 = questions[Math.floor(Math.random() * (50))]
+  // const question_4 = questions[Math.floor(Math.random() * (50))]
+  // const question_5 = questions[Math.floor(Math.random() * (50))]
+
+  shuffleArray(questions)
+
   res.json(
-    questions[Math.floor(Math.random() * (50))]
-  );
-});
-
-router.post('/', async (req, res) => {
-  const { answer } = req.body;
-  const questionId = req.session.questionId;
-  const question = await Question.findByPk(questionId, {
-    include: [Answer],
-  });
-
-  console.log('question:', question);
-  console.log('questionId:', questionId);
-
-  const selectedAnswer = question.answers.find(
-    (ans) => ans.answer_choice === answer
+    questions
   );
 
-  console.log('selectedAnswer:', selectedAnswer);
-
-  let isCorrect;
-  if (selectedAnswer.is_correct) {
-    req.session.score++;
-    isCorrect = true;
-  } else {
-    isCorrect = false;
-  }
-
-  const newQuestion = await Question.findOne({
-    where: {
-      id: { [Op.not]: questionId },
-    },
-    include: [Answer],
-    order: sequelize.random(),
-  });
-
-  console.log('newQuestion:', newQuestion);
-
-  if (!newQuestion) {
-    res.send('There are no more questions in the database.');
-    return;
-  }
-
-  shuffleArray(newQuestion.answers);
-
-  req.session.questionId = newQuestion.id;
-
-  res.render('quiz', {
-    question: newQuestion,
-    score: req.session.score,
-    isCorrect,
-  });
+  console.log("response sent")
 });
+
+// router.post('/', async (req, res) => {
+//   const { answer } = req.body;
+//   const questionId = req.session.questionId;
+//   const question = await Question.findByPk(questionId, {
+//     include: [Answer],
+//   });
+
+//   console.log('question:', question);
+//   console.log('questionId:', questionId);
+
+//   const selectedAnswer = question.answers.find(
+//     (ans) => ans.answer_choice === answer
+//   );
+
+//   console.log('selectedAnswer:', selectedAnswer);
+
+//   let isCorrect;
+//   if (selectedAnswer.is_correct) {
+//     req.session.score++;
+//     isCorrect = true;
+//   } else {
+//     isCorrect = false;
+//   }
+
+//   const newQuestion = await Question.findOne({
+//     where: {
+//       id: { [Op.not]: questionId },
+//     },
+//     include: [Answer],
+//     order: sequelize.random(),
+//   });
+
+//   console.log('newQuestion:', newQuestion);
+
+//   if (!newQuestion) {
+//     res.send('There are no more questions in the database.');
+//     return;
+//   }
+
+//   shuffleArray(newQuestion.answers);
+
+//   req.session.questionId = newQuestion.id;
+
+//   res.render('quiz', {
+//     question: newQuestion,
+//     score: req.session.score,
+//     isCorrect,
+//   });
+// });
 
 module.exports = router;
